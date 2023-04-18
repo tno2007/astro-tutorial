11342dda3dde1ebbb53ac0946c59479959be7cd4
Lyall <lyall.vanderlinde@iriesoft.net>
2023-04-18 11:05:30 +0200

    rename project

---
+++ src/components/vue/calculator.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed, reactive } from "vue";
+
+const data = reactive({
+  a: 0,
+  b: 0,
+  c: 0,
+});
+
+//const answer = computed(() => data.a + data.b);
+
+const solve = () => (data.c = data.a + data.b);
+</script>
+
+<template>
+  <div>
+    <div><input type="text" v-model="data.a" /></div>
+    <div><input type="text" v-model="data.b" /></div>
+    <button @click="solve">Solve</button>
+    <p>Result</p>
+    <div>{{ data.c }}</div>
+  </div>
+</template>
